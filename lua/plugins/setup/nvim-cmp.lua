fec2c265d8a7d88bfc4f32b3f76b6b312a41e7d6
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-02-15 22:32:34 -0300

    feat: Split `lspconfig` & `cmp` configurations

--- lua/plugins/setup/nvim-cmp.lua
+++ lua/plugins/setup/nvim-cmp.lua
@@ -62,12 +62,19 @@ cmp.setup(
 
 			{ -- LSP
 				name = 'nvim_lsp',
-				max_item_count = 20,
+				max_item_count = 25,
 			},
 
 			{ -- LSP signatures
 				name = 'nvim_lsp_signature_help',
-				max_item_count = 3,
+				max_item_count = 5,
+			},
+
+			{ -- Omnifunc
+				name = 'omni',
+				priority = 10,
+				keyword_length = 3,
+				max_item_count = 25
 			},
 
 			{ -- Path
@@ -108,88 +115,12 @@ cmp.setup.cmdline('/', {
 
 -- `:` cmdline setup.
 cmp.setup.cmdline(':', {
-	sources = {
-		{ -- Command line
-			name = 'cmdline',
-			keyword_length = 5,
-			max_item_count = 10
-		},
-	}
-})
-
--- `tex` setup.
-cmp.setup.filetype({'tex', 'bib'}, {
 		sources = {
-			{ -- Omnifunc
-				name = 'omni',
-				priority = 10,
-				keyword_length = 3,
+			{ -- Command line
+				name = 'cmdline',
+				keyword_length = 5,
 				max_item_count = 10
 			},
 		}
 	}
 )
-
--- LSP settings
-local lspconfig = require('lspconfig')
-local on_attach = function(_, bufnr)
-	local opts = { noremap = true, silent = true }
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-	vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
-end
-
--- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
--- Enable the following language servers with default setups
-local servers = { 'clangd', 'julials', 'bashls', 'cmake'}
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
-end
-
--- Python
-lspconfig['pyright'].setup(
-	{
-		on_attach = on_attach,
-		capabilities = capabilities,
-		-- settings = {
-		-- 	python = {
-		-- 		venvPath = "/home/carlos/documents/git/environments",
-		-- 		venv = { "dataScience" },
-		-- 	}
-		-- }
-	}
-)
-
--- Lua
-lspconfig['sumneko_lua'].setup(
-	{
-		on_attach = on_attach,
-		capabilities = capabilities,
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { 'vim' }
-				}
-			}
-		}
-	}
-)
-
-vim.cmd("LspStart")
