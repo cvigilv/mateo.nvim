58529171c7694ee6c0de4e4f03abed850c7d7c91
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-04-22 17:12:24 -0400

    chore(plugin): Clean-up `tokyonight.nvim` config

--- lua/plugin/setup/tokyonight.lua
+++ lua/plugin/setup/tokyonight.lua
@@ -1,29 +1,23 @@
 local execute = vim.api.nvim_command
 
--- Setup
+-- Setup {{{
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_terminal_colors = false
+vim.g.tokyonight_italic_comments = false
+vim.g.tokyonight_italic_keyworks = false
+vim.g.tokyonight_italic_functions = false
+vim.g.tokyonight_italic_variables = false
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_hide_inactive_statusline = true
 vim.g.tokyonight_sidebars = {"packer", "terminal", "NvimTree"}
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_comments = true
-vim.g.tokyonight_hide_inactive_statusline = false
-
--- More contrast for ´Normal´ group and respect the ´background´ variable
-execute [[
-	function s:TokyoNight_bg()
-		let l:bg='set background?'
-		if l:bg == 'dark'
-			highlight Normal guifg='#EDF0FC'
-		elif l:bg == 'light'
-			highlight Normal guifg='#1A1B26'
-		endif
-	endfunction
-
-	augroup hl_TokyoNight
-		" `tokyonight`
-		autocmd ColorScheme tokyonight call s:TokyoNight_bg()
-		autocmd ColorScheme tokyonight highligh link CursorLineNr WarningMsg
-	augroup END
-	call s:TokyoNight_bg()
-]]
+vim.g.tokyonight_transparent_sidebar = false
+vim.g.tokyonight_dark_sidebar = true
+vim.g.tokyonight_dark_float = true
+vim.g.tokyonight_colors = {}
+vim.g.tokyonight_day_brightness = 0.8
+vim.g.tokyonight_lualine_bold = true
+-- }}}
 
 execute [[ colorscheme tokyonight ]]
+execute [[ highlight Normal guifg='#EDF0FC' guibg=none ]]
+execute [[ highligh link CursorLineNr WarningMsg ]]
