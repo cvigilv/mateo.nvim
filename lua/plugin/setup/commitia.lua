8a2a24574ab5ca63ebc061e3643538759221cc0f
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-04-22 17:27:04 -0400

    feat(plugin): testing some Lua in `commitia.vim`

--- lua/plugin/setup/commitia.lua
+++ lua/plugin/setup/commitia.lua
@@ -1,14 +1,16 @@
 local execute = vim.api.nvim_command
 
-execute [[
-	let g:commitia_open_only_vim_starting = 0
-	let g:commitia_status_window_min_height = 20
+vim.g.commitia_open_only_vim_starting = 0
+vim.g.commitia_status_window_min_height = 20
 
-	let g:committia_hooks = {}
+execute [[
+  " let g:commitia_open_only_vim_starting = 0
+  " let g:commitia_status_window_min_height = 20
+  let g:committia_hooks = {}
 	function! g:committia_hooks.edit_open(info)
 		" Scroll the diff window from insert mode
 		" Map <C-n> and <C-p>
-		imap <buffer><C-n> <Plug>(committia-scroll-diff-down)
-		imap <buffer><C-p> <Plug>(committia-scroll-diff-up)
+		imap <buffer><C-f> <Plug>(committia-scroll-diff-down)
+		imap <buffer><C-b> <Plug>(committia-scroll-diff-up)
 	endfunction
 ]]
