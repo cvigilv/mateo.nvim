997f4a30d03346b26e215597f3cdbf4ad79b6904
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-04-23 13:01:00 -0400

    feat(plugin): new `nvim-cmp` configuration
    Neovim 0.7 broke my previous configuration.

--- lua/plugin/setup/nvim-cmp.lua
+++ lua/plugin/setup/nvim-cmp.lua
@@ -1,117 +1,104 @@
 local cmp = require('cmp')
 
-local function check_backspace()
-  local col = vim.fn.col '.' - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
-end
-
-local feedkeys = vim.fn.feedkeys
-local replace_termcodes = vim.api.nvim_replace_termcodes
-local backspace_keys = replace_termcodes('<tab>', true, true, true)
-
 -- Global setup.
-cmp.setup(
-	{
-		-- Completion menu rules
-		completion = { completeopt = 'menuone,preview' },
+cmp.setup({
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
 
-		-- Snippet integration
-		snippet = {},
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+  }),
+  sources = cmp.config.sources({
+    { -- nvim Lua
+      name = 'nvim_lua',
+      max_item_count = 10,
+      view = {
+        entries = {
+          name = 'custom',
+          selection_order = 'near_cursor'
+        }
+      },
+    },
 
-		-- Items formatting
-		formatting = {},
+    { -- LSP
+      name = 'nvim_lsp',
+      max_item_count = 25,
+      view = {
+        entries = {
+          name = 'custom',
+          selection_order = 'near_cursor'
+        }
+      },
+    },
 
-		-- Key mappings
-		mapping = {
-			-- ´nvim-cmp´ specific mappings
-			['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-			['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-			['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-			['<C-e>'] = cmp.mapping({
-				i = cmp.mapping.abort(),
-				c = cmp.mapping.close(),
-			}),
-			['<CR>'] = cmp.mapping.confirm({ select = true }),
-			['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    { -- LSP signatures
+      name = 'nvim_lsp_signature_help',
+      max_item_count = 5,
+      view = {
+        entries = {
+          name = 'custom',
+          selection_order = 'near_cursor'
+        }
+      },
+    },
 
-			-- Tab completion mappings
-			['<Tab>'] = function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif check_backspace() then
-					feedkeys(backspace_keys, 'n')
-				else
-					fallback()
-				end
-			end,
-			['<S-Tab>'] = function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				else
-					fallback()
-				end
-			end,
-		},
+    { -- Omnifunc
+      name = 'omni',
+      keyword_length = 3,
+      max_item_count = 25,
+      view = {
+        entries = {
+          name = 'custom',
+          selection_order = 'near_cursor'
+        }
+      },
+    },
 
-		-- Autocompletion sources
-		sources = {
-			{ -- nvim Lua
-				name = 'nvim_lua',
-				max_item_count = 10,
-			},
-
-			{ -- LSP
-				name = 'nvim_lsp',
-				max_item_count = 25,
-			},
-
-			{ -- LSP signatures
-				name = 'nvim_lsp_signature_help',
-				max_item_count = 5,
-			},
-
-			{ -- Omnifunc
-				name = 'omni',
-				keyword_length = 3,
-				max_item_count = 25
-			},
-
-			{ -- Path
-				name = 'path',
-				max_item_count = 5,
-			},
-		},
-
-		experimental = {
-			ghost_text = true,
-			native_menu = false
-		}
-	}
-)
+    { -- Path
+      name = 'path',
+      max_item_count = 5,
+      view = {
+        entries = {
+          name = 'custom',
+          selection_order = 'near_cursor'
+        }
+      },
+    },
+  }),
+})
 
 -- `/` cmdline setup.
 cmp.setup.cmdline('/', {
-		sources = {
-			{ -- Buffer
-				name = 'buffer',
-				keyword_length = 5,
-				max_item_count = 10,
-				option = {
-					keyword_length = 3,
-				},
-			},
-		},
-	}
-)
+  sources = {
+    { -- Buffer
+      name = 'buffer',
+      keyword_length = 5,
+      max_item_count = 10,
+      option = {
+        keyword_length = 3,
+      },
+    },
+  },
+})
 
 -- `:` cmdline setup.
 cmp.setup.cmdline(':', {
-		sources = {
-			{ -- Command line
-				name = 'cmdline',
-				keyword_length = 5,
-				max_item_count = 10
-			},
-		}
-	}
-)
+  sources = {
+    { -- Command line
+      name = 'cmdline',
+      keyword_length = 5,
+      max_item_count = 10,
+    },
+  }
+})
