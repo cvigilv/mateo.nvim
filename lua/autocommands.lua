b42b0752cd9f178dab99878833161fd73c22113b
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-04-30 01:44:36 -0400

    feat(autocmd): Highlight yanked text

---
+++ lua/autocommands.lua
@@ -0,0 +1,8 @@
+-- Highlight yanked text
+vim.api.nvim_create_autocmd(
+  {"TextYankPost"},
+  {
+    pattern = {"*"},
+    callback = function() vim.highlight.on_yank({higroup="IncSearch", timeout=1000}) end,
+  }
+)
