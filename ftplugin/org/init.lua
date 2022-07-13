2fa7c5f0ec51c638282a32b2411c475993668436
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-07-12 20:05:41 -0400

    feat(ftplugin): add some `org` settings

--- ftplugin/org/init.lua
+++ ftplugin/org/init.lua
@@ -1,3 +1,13 @@
+-- Conceal
 vim.opt.suffixesadd = ".org"
 vim.opt.conceallevel = 3
 vim.opt.concealcursor = "nc"
+
+-- Tabulate behaviour
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth  = 4
+vim.opt.tabstop     = 4
+vim.opt.expandtab   = true
+
+-- Others
+vim.wo.colorcolumn  = "92"
