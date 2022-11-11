5e6b9085330820a6fb0c15bdaa314781a9b055ff
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-11-11 14:54:27 +0100

    fix(lsp): correct default capabilities for `nvim-cmp`

--- lua/plugin/setup/nvim-lspconfig.lua
+++ lua/plugin/setup/nvim-lspconfig.lua
@@ -37,7 +37,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Server specific configuration
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Julia {{{
 require 'lspconfig'.julials.setup{
