861ba1aa6bfa9fd5bae408b2af12cca26ac231b9
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-11-11 15:52:15 +0100

    feat(packages):  as package managment

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require("mateo.defaults")
+require("mateo.packages")
---
+++ lua/mateo/packages/init.lua
@@ -0,0 +1,24 @@
+-- Boostrapping
+-- NOTE: This snippet will automatically install and setup `packer.nvim`
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer()
+
+-- Package management
+-- NOTE: For more information about `packer` and it's use, please refer to `https://github.com/wbthomason/packer.nvim`
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+    warn("PackerSync is running, restart after the process is done!")
+  end
+end)
