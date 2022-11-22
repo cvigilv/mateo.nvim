5475ad0bc666c4bbf937873b15f41ee6279e3fcf
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-11-22 11:25:24 +0100

    mend

---
+++ skeletons/skeleton.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#title           :
+#description     :
+#author          :Carlos Vigil Vásquez
+#date            :
+#version         :
+#notes           :
+#copyright       :Copyright (C) 2022 Carlos Vigil Vásquez (carlos.vigil.v@gmail.com)
+#license         :Permission to copy and modify is granted under the MIT license
+
+
