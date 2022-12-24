f1d6a1181bfcb0e8d67c2aae791d4af03620cc49
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2022-12-24 10:32:23 +0100

    refactor(ui): clean-up statusline

--- after/plugin/ui.lua
+++ after/plugin/ui.lua
@@ -8,8 +8,8 @@ require("tokyonight").setup({
   styles = {
     comments = { italic = false },
     keywords = { italic = false },
-    functions = { italic = false},
-    variables = { italic = false},
+    functions = { italic = false },
+    variables = { italic = false },
     sidebars = "dark",
     floats = "dark",
   },
@@ -21,12 +21,10 @@ require("tokyonight").setup({
 
   on_colors = function(colors)
     colors.fg = '#EDF0FC'
-    colors.border = '#24283b'
+    colors.border = '#121317'
   end,
-
-  on_highlights = function(highlights, colors) end,
 })
-vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd [[colorscheme tokyonight-night]]
 -- }}}
 -- statusline {{{
 local lualine = require('lualine')
@@ -34,9 +32,10 @@ local lualine = require('lualine')
 -- General configuration
 -- Color table
 local colors = {
-  bg       = '#24283b',
+  bg       = '#121317',
   fg       = '#EDF0FC',
   light_bg = '#464d72',
+  light_fg = '#DCE2F9',
   yellow   = '#ECBE7B',
   cyan     = '#008080',
   darkblue = '#081633',
@@ -84,189 +83,233 @@ local config = {
 }
 
 -- Helper functions
-local spacer = {
-  function()
-    return ' '
-  end,
-  color = {},
-  padding = { left = 0, right = 1 },
-}
+local spacer = function(char)
+  return { function() return char end,
+    color = {},
+    padding = { left = 0, right = 1 },
+  }
+end
 
 -- Inserts a component in lualine_c at left section
-local function ins_left(component, keep)
+local function insert_left(component, keep)
   table.insert(config.sections.lualine_c, component)
 
   keep = keep or false
   if (keep) then
     table.insert(config.inactive_sections.lualine_c, component)
   else
-    table.insert(config.inactive_sections.lualine_c, spacer)
+    table.insert(config.inactive_sections.lualine_c, spacer(' '))
   end
 end
 
 -- Inserts a component in lualine_x ot right section
-local function ins_right(component, keep)
+local function insert_right(component, keep)
   table.insert(config.sections.lualine_x, component)
 
   keep = keep or false
   if (keep) then
     table.insert(config.sections.lualine_x, component)
   else
-    table.insert(config.inactive_sections.lualine_x, spacer)
+    table.insert(config.inactive_sections.lualine_x, spacer(' '))
   end
 end
 
--- Statusline
--- Left section
--- Divider
-ins_left({
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue }, -- Sets highlighting of component
-  padding = { left = 0, right = 1 }, -- We don't need space before this
-}, false)
-
--- Mode component
-ins_left {
-  function()
-    return string.upper(vim.fn.mode())
-  end,
-  color = function()
-    -- auto change color according to neovims mode
-    local mode_color = {
-      n = colors.red,
-      i = colors.green,
-      v = colors.blue,
-      [''] = colors.blue,
-      V = colors.blue,
-      c = colors.magenta,
-      no = colors.red,
-      s = colors.orange,
-      S = colors.orange,
-      [''] = colors.orange,
-      ic = colors.yellow,
-      R = colors.violet,
-      Rv = colors.violet,
-      cv = colors.red,
-      ce = colors.red,
-      r = colors.cyan,
-      rm = colors.cyan,
-      ['r?'] = colors.cyan,
-      ['!'] = colors.red,
-      t = colors.red,
-    }
-    return { fg = mode_color[vim.fn.mode()] }
-  end,
-  padding = { right = 1 },
-}
-
--- Filename
-ins_left({
-  'filename',
-  file_status = true,
-  color = { fg = colors.magenta, gui = 'bold' },
-}, true)
-
--- Git
-ins_left {
-  'branch',
-  icon = '⎇',
-  color = { fg = colors.violet },
-  cond = function () return vim.fn.winwidth(0) > 70 end
-}
-ins_left {
-  'diff',
-  symbols = {
-    added = '+',
-    modified = '~',
-    removed = '-'
+-- Implement modules
+local left = {
+  { -- Mode
+    function()
+      return " " .. string.upper(vim.fn.mode())
+    end,
+    color = function()
+      local mode_color = {
+        n = colors.red,
+        i = colors.green,
+        v = colors.blue,
+        [''] = colors.blue,
+        V = colors.blue,
+        c = colors.magenta,
+        no = colors.red,
+        s = colors.orange,
+        S = colors.orange,
+        [''] = colors.orange,
+        ic = colors.yellow,
+        R = colors.violet,
+        Rv = colors.violet,
+        cv = colors.red,
+        ce = colors.red,
+        r = colors.cyan,
+        rm = colors.cyan,
+        ['r?'] = colors.cyan,
+        ['!'] = colors.red,
+        t = colors.red,
+      }
+      return { fg = colors.bg, bg = mode_color[vim.fn.mode()] }
+    end,
+    padding = { left = 0, right = 1 },
   },
-  diff_color = {
-    added = { fg = colors.green },
-    modified = { fg = colors.orange },
-    removed = { fg = colors.red },
+  { -- File flags
+    '%r%h',
+    color = { fg = colors.light_bg, gui = 'bold' },
+    padding = { left = 1, right = 1 },
   },
-  cond = function () return vim.fn.winwidth(0) > 70 end
+  { -- Filename
+    'filename',
+    color = { fg = colors.light_fg, gui = 'bold' },
+    padding = { left = 0, right = 1 },
+  },
+  { -- Modified flag
+    '%M',
+    color = { fg = colors.light_bg },
+    padding = { left = 0, right = 1 },
+  },
 }
 
--- Mid section
-ins_left {
-  function()
-    return '%='
-  end,
-  color = { fg = colors.light_bg },
-  cond = function() return vim.fn.winwidth(0) > 120 end
-}
-
--- Right section
--- Diagnostics
-ins_right {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  symbols = {
-    error = 'x',
-    warn = '!',
-    info = '?',
-    hint = '*'
+local middle = {
+  {
+    function() return '·' end,
+    color = { fg = colors.light_bg },
+    padding = { left = 1, right = 0 },
+    cond = function() return vim.fn.winwidth(0) > 70 end
   },
-  diagnostics_color = {
-    color_error = { fg = colors.red },
-    color_warn = { fg = colors.yellow },
-    color_info = { fg = colors.cyan },
-    color_hint = { fg = colors.cyan },
+  { -- Git branch
+    'branch',
+    icon = '⎇ ',
+    color = { fg = colors.violet },
+    cond = function() return vim.fn.winwidth(0) > 70 end
   },
-  cond = function () return vim.fn.winwidth(0) > 55 end
-}
-
--- LSP
-ins_right {
-  function()
-    local msg = ''
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return '■ ' .. client.name .. ' ■'
+  { -- Git diff
+    'diff',
+    symbols = {
+      added = '+',
+      modified = '~',
+      removed = '-'
+    },
+    diff_color = {
+      added = { fg = colors.green },
+      modified = { fg = colors.orange },
+      removed = { fg = colors.red },
+    },
+    padding = { left = 0, right = 0 },
+    cond = function() return vim.fn.winwidth(0) > 70 end
+  },
+  {
+    function() return ' ' end,
+    color = { fg = colors.light_bg },
+    padding = { left = 0, right = 0 },
+    cond = function() return vim.fn.winwidth(0) > 70 end
+  },
+  { -- LSP attached
+    function()
+      local msg = ''
+      local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+      local clients = vim.lsp.get_active_clients()
+      if next(clients) == nil then
+        return msg
       end
-    end
-    return msg
-  end,
-  icon = '',
-  color = { fg = colors.fg },
-  cond = function() return vim.fn.winwidth(0) > 55 end
+      for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+          return client.name
+        end
+      end
+      return msg
+    end,
+    icon = '⌕',
+    color = { fg = colors.violet },
+    padding = { left = 1, right = 1 },
+    cond = function() return vim.fn.winwidth(0) > 55 end
+  },
+  { -- LSP diagnostics
+    'diagnostics',
+    sources = { 'nvim_diagnostic' },
+    symbols = {
+      error = 'x',
+      warn = '!',
+      info = '?',
+      hint = '*'
+    },
+    diagnostics_color = {
+      color_error = { fg = colors.red },
+      color_warn = { fg = colors.yellow },
+      color_info = { fg = colors.cyan },
+      color_hint = { fg = colors.cyan },
+    },
+    padding = { left = 0, right = 0 },
+    cond = function() return vim.fn.winwidth(0) > 55 end
+  },
+  {
+    function() return '·' end,
+    color = { fg = colors.light_bg },
+    padding = { left = 1, right = 0 },
+    cond = function() return vim.fn.winwidth(0) > 55 end
+  }
 }
 
--- Cursor location
-ins_right {
-  '%l/%L',
-  icon = 'ℓ',
-  color = { fg = colors.light_bg },
-  cond = function() return vim.fn.winwidth(0) > 120 end
-}
-ins_right {
-  function()
-    local _, c = unpack(vim.api.nvim_win_get_cursor(0))
-    local ncols = string.len(vim.api.nvim_get_current_line())
-    return string.format('%d/%d', c + 1, ncols)
-  end,
-  icon = '∁',
-  color = { fg = colors.light_bg },
-  cond = function() return vim.fn.winwidth(0) > 80 end
+right = {
+  { -- Filetype
+    function() return vim.bo.filetype end,
+    icon = '⌨',
+    color = { fg = colors.light_bg },
+    padding = { left = 0, right = 0 }
+  },
+  {
+    function() return '·' end,
+    color = { fg = colors.light_bg },
+  },
+  { -- Line number
+    '%l/%L',
+    icon = 'ℓ',
+    color = { fg = colors.light_bg },
+    cond = function() return vim.fn.winwidth(0) > 80 end,
+    padding = { left = 0, right = 0 }
+  },
+  {
+    function() return '·' end,
+    color = { fg = colors.light_bg },
+    cond = function() return vim.fn.winwidth(0) > 80 end,
+  },
+  { -- Column
+    function()
+      local _, c = unpack(vim.api.nvim_win_get_cursor(0))
+      local ncols = string.len(vim.api.nvim_get_current_line())
+      return string.format('%d/%d', c + 1, ncols)
+    end,
+    icon = '∁',
+    color = { fg = colors.light_bg },
+    cond = function() return vim.fn.winwidth(0) > 80 end,
+    padding = { left = 0, right = 0 }
+  },
+  {
+    function() return '·' end,
+    color = { fg = colors.light_bg },
+    cond = function() return vim.fn.winwidth(0) > 80 end,
+  },
+  { -- Filesize
+    'filesize',
+    icon = '⍵',
+    color = { fg = colors.light_bg },
+    cond = function() return vim.fn.winwidth(0) > 80 end,
+    padding = { left = 0, right = 2 },
+  },
 }
 
--- Divider
-ins_right({
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue },
-  padding = { left = 1 },
-}, false)
+
+-- Insert modules
+for _, tbl in ipairs(left) do
+  insert_left(tbl, false)
+end
+
+insert_left({ function() return '%=' end,
+  color = { fg = colors.light_bg },
+})
+
+for _, tbl in ipairs(middle) do
+  insert_left(tbl, false)
+end
+
+for _, tbl in ipairs(right) do
+  insert_right(tbl, false)
+end
 
 lualine.setup(config)
--- }}}
+-- }}f
