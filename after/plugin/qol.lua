14d5e54fd2417df872aa28769e05a4ac11200ded
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-01-26 14:13:02 -0300

    feat(misc): add keymaps to combat HHKB latency

--- after/plugin/qol.lua
+++ after/plugin/qol.lua
@@ -115,3 +115,21 @@ vim.keymap.set(
   { silent = true, noremap = true }
 )
 -- }}}
+-- misc {{{
+vim.api.nvim_create_user_command(
+  'Q',
+  'q',
+  {}
+)
+vim.api.nvim_create_user_command(
+  'W',
+  'w',
+  {}
+)
+vim.api.nvim_create_user_command(
+  'Wqa',
+  'wqa',
+  {}
+)
+
+-- }}}
