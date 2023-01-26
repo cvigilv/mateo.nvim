2a0e67ced3b884a72d5bbc81a3ae727e1dbdbaaa
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-01-26 14:07:23 -0300

    feat(lsp): julia precompiled LSP
    Based on https://discourse.julialang.org/t/neovim-languageserver-jl/37286/83

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -69,6 +69,14 @@ lsp.configure('sumneko_lua', {
     }
   }
 })
+lsp.configure('julials', {
+  on_new_config = function(new_config, _)
+    local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+    if require 'lspconfig'.util.path.is_file(julia) then
+      new_config.cmd[1] = julia
+    end
+  end
+})
 
 lsp.setup()
 vim.keymap.set("n", "<M-k>", function() vim.lsp.buf.signature_help() end, { noremap = true, silent = true })
