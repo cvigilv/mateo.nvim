e3dd71dc33839fd9a8d14ad7f07d54d5cfa21fb6
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-04-08 22:46:26 -0400

    feat(qol): add `todo-comments.nvim`

--- lua/plugin/qol.lua
+++ lua/plugin/qol.lua
@@ -125,6 +125,18 @@ return {
     end
   },
   -- }}}
+  -- todo-comments {{{
+  {
+    'folke/todo-comments.nvim',
+    config = function()
+      require("todo-comments").setup(
+        {
+          signs = false,
+        }
+      )
+    end
+  },
+  -- }}}
   -- undotree {{{
   {
     'mbbill/undotree',
