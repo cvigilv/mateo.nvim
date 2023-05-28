6027a270010236a0e8eb3c9ea7b4ae208bc345df
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-05-28 19:50:43 -0400

    feat(julia): add keymap to open REPL

--- after/ftplugin/julia/init.lua
+++ after/ftplugin/julia/init.lua
@@ -3,3 +3,6 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth  = 4
 vim.opt.tabstop     = 4
 vim.opt.expandtab   = true
+
+-- REPL
+vim.keymap.set("n", "<leader>R", ":!tmux split-window -h 'exec julia'<CR>", { noremap=true })
