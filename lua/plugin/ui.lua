d9a827438a796a47dc75070c3e868928ba16295f
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-05-28 19:45:22 -0400

    feat(ui): cleanup status line

--- lua/plugin/ui.lua
+++ lua/plugin/ui.lua
@@ -103,52 +103,48 @@ return {
   -- }, -- }}}
   -- statusline {{{
   {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
+    dependencies = "rktjmp/lush.nvim",
     config = function()
-      local lualine = require('lualine')
+      local lualine = require("lualine")
+      local lush = require("lush")
+      local hsl = lush.hsluv
 
       -- General configuration
-      -- Color table
       local colors = {
-        fg       = vim.api.nvim_eval('synIDattr(synIDtrans(hlID("Normal")), "fg#")'),
-        bg       = vim.api.nvim_eval('synIDattr(synIDtrans(hlID("Normal")), "bg#")'),
-        yellow   = '#ECBE7B',
-        cyan     = '#008080',
-        darkblue = '#081633',
-        green    = '#98be65',
-        orange   = '#FF8800',
-        violet   = '#a9a1e1',
-        magenta  = '#c678dd',
-        blue     = '#51afef',
-        red      = '#ec5f67',
+        fg = hsl("#F5F5F5"),
+        bg = hsl("#09090C"),
+        yellow = "#ECBE7B",
+        cyan = "#008080",
+        darkblue = "#081633",
+        green = "#98BE65",
+        orange = "#FF8800",
+        violet = "#A9A1E1",
+        magenta = "#C678DD",
+        blue = "#51AFEF",
+        red = "#EC5F67",
       }
 
-      -- Config
-      local config = {
-        options = {
-          -- Disable lualine in certain filetypes
-          disabled_filetypes = { "packer", "starter" },
-          -- Disable sections and component separators
-          component_separators = '',
-          section_separators = '',
-          theme = {
-            normal = { c = { fg = colors.fg, bg = colors.bg } },
-            inactive = { c = { fg = colors.fg, bg = colors.bg } },
-          },
-        },
-        sections = nil,
-        inactive_sections = nil,
+      local ignore_filetype = {
+        "help",
+        "NvimTree",
+        "orgagenda",
+        "quickfix",
+        "loclist",
+        "packer",
+        "starter",
+        "esqueleto.ivy.selection",
       }
 
-      local ignore_filetype = { 'help', 'NvimTree', 'orgagenda', 'quickfix', 'loclist', 'packer', 'starter',
-        'esqueleto.ivy.selection' }
       local condition = function()
         local current_filetype = vim.bo.filetype
 
+        -- Window width
         if vim.fn.winwidth(0) < 70 then
           return false
         end
 
+        -- File type
         for _, ignored in pairs(ignore_filetype) do
           if current_filetype == ignored then
             return false
@@ -158,84 +154,114 @@ return {
         return true
       end
 
+      -- Config
+      local config = {
+        options = {
+          -- Disable lualine in certain filetypes
+          disabled_filetypes = ignore_filetype,
+
+          -- Disable sections and component separators
+          component_separators = "",
+          section_separators = "",
+
+          -- Configure theme
+          theme = {
+            normal = { c = { fg = colors.fg.hex, bg = colors.bg.sa(10).li(5).hex } },
+            inactive = { c = { fg = colors.fg.hex, bg = colors.bg.hex } },
+          },
+        },
+
+        -- Initialize empty sections
+        sections = nil,
+        inactive_sections = nil,
+      }
+
+      -- Setup lualine sections
       local sections = {
         lualine_a = {},
         lualine_b = {},
         lualine_c = {
           {
-            -- Git branch
-            'branch',
-            icon = '⎇',
+            -- Filename
+            "filename",
             padding = { left = 2, right = 1 },
-            cond = condition
+          },
+        },
+        lualine_x = {
+          {
+            -- Git branch
+            "branch",
+            icon = "⎇",
+            padding = { left = 0, right = 0 },
+            color = { fg = colors.fg.darken(20).hex },
+            cond = condition,
           },
           {
             -- Git diff
-            'diff',
+            "diff",
             symbols = {
-              added = '+',
-              modified = '~',
-              removed = '-'
+              added = "+",
+              modified = "~",
+              removed = "-",
             },
             diff_color = {
               added = { fg = colors.green },
               modified = { fg = colors.orange },
               removed = { fg = colors.red },
             },
-            padding = { left = 0, right = 2 },
+            padding = { left = 1, right = 0 },
             cond = condition,
           },
-
           {
-            -- Center section
-            function() return '%=' end,
-            padding = { left = 0, right = 0 },
-          },
-
-          {
-            -- Filename
-            '%r%t%m',
+            function()
+              return "∘"
+            end,
             padding = { left = 1, right = 1 },
+            cond = condition,
           },
-        },
-        lualine_x = {
+          {
+            -- LSP attached
+            function()
+              local msg = "⚙ "
+              local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+              local clients = vim.lsp.get_active_clients()
+
+              -- Return nothing if no LSP server
+              if next(clients) == nil then
+                return ""
+              end
+
+              -- return non-"null-ls" servers
+              for _, client in ipairs(clients) do
+                if client.name ~= "null-ls" then
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    msg = msg .. client.name
+                  end
+                end
+              end
+              if vim.tbl_count(clients) > 0 then
+                msg = msg .. "+" .. vim.tbl_count(clients) - 1
+              end
+              return msg
+            end,
+            color = { fg = colors.fg.darken(20).hex },
+            padding = { left = 0, right = 0 },
+            cond = condition,
+          },
           {
             -- LSP diagnostics
-            'diagnostics',
-            sources = { 'nvim_diagnostic' },
-            symbols = {
-              error = 'x',
-              warn = '!',
-              info = '?',
-              hint = '*'
-            },
+            "diagnostics",
+            sources = { "nvim_lsp", "nvim_diagnostic" },
+            symbols = { error = "x", warn = "!", info = "?", hint = "*" },
             diagnostics_color = {
               color_error = { fg = colors.red },
               color_warn = { fg = colors.yellow },
               color_info = { fg = colors.cyan },
               color_hint = { fg = colors.cyan },
             },
-            padding = { left = 2, right = 1 },
-            cond = condition,
-          },
-          {
-            -- LSP attached
-            function()
-              local msg = ''
-              local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-              local clients = vim.lsp.get_active_clients()
-              if next(clients) == nil then
-                return msg
-              end
-              for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                  return client.name
-                end
-              end
-              return msg
-            end,
-            icon = '⌕',
+            always_visible = true,
+            update_in_insert = true,
             padding = { left = 1, right = 2 },
             cond = condition,
           },
@@ -248,7 +274,7 @@ return {
       config.inactive_sections = sections
 
       lualine.setup(config)
-    end
+    end,
   },
   -- }}}
   -- which-key {{{
