11fb27b56a23b6833c9bdada3506256a3306ec31
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-06-01 19:56:12 -0400

    fix(skeletons): fix year

--- skeletons/julia/cli.jl
+++ skeletons/julia/cli.jl
@@ -5,7 +5,7 @@
 #date            :
 #version         :
 #notes           :
-#copyright       :Copyright (C) 2022 Carlos Vigil Vásquez (carlos.vigil.v@gmail.com)
+#copyright       :Copyright (C) 2023 Carlos Vigil Vásquez (carlos.vigil.v@gmail.com)
 #license         :Permission to copy and modify is granted under the MIT license
 
 using ArgParse
