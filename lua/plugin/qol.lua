cda127093b1b661bde01c008c963407806a8f314
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-08-31 17:53:24 -0400

    feat(plugin): add numb

--- lua/plugin/qol.lua
+++ lua/plugin/qol.lua
@@ -110,6 +110,22 @@ return {
     end
   },
   -- }}}
+  -- numb.nvim {{{
+  {
+    "nacro90/numb.nvim",
+    config = function()
+      require("numb").setup(
+        {
+          show_numbers = true,
+          show_cursorline = true,
+          hide_relativenumbers = true,
+          number_only = false,
+          centered_peeking = true,
+        }
+      )
+    end,
+  },
+  -- }}}
   -- overlength {{{
   {
     "lcheylus/overlength.nvim",
