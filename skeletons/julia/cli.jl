e3b91d8f3c17e65c90eb2ae657d9d6ffb4deedc1
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-09-02 21:17:28 -0400

    feat(skeletons): use wildcard expansion

--- skeletons/julia/cli.jl
+++ skeletons/julia/cli.jl
@@ -1,12 +1,12 @@
 #!/usr/local/bin/julia
-#title           :
-#description     :
-#author          :Carlos Vigil Vásquez
-#date            :
-#version         :
-#notes           :
-#copyright       :Copyright (C) 2023 Carlos Vigil Vásquez (carlos.vigil.v@gmail.com)
-#license         :Permission to copy and modify is granted under the MIT license
+#title      : 
+#description: 
+#author     : ${gh-user}
+#date       : ${date}
+#version    : ${date}a
+#notes      : 
+#copyright  : Copyright (C) ${year} ${gh-user}  (${gh-email})
+#license    : Permission to copy and modify is granted under the MIT license
 
 using ArgParse
 
--- skeletons/julia/tests.jl
+++ skeletons/julia/tests.jl
@@ -1,17 +1,16 @@
 #!/usr/local/bin/julia
-#title           :
-#description     :
-#author          :Carlos Vigil Vásquez
-#date            :
-#version         :
-#notes           :
-#copyright       :Copyright (C) 2023 Carlos Vigil Vásquez (carlos.vigil.v@gmail.com)
-#license         :Permission to copy and modify is granted under the MIT license
+#title      : 
+#description: 
+#author     : ${gh-user}
+#date       : ${date}
+#version    : ${date}a
+#notes      : 
+#copyright  : Copyright (C) ${year} ${gh-user}  (${gh-email})
+#license    : Permission to copy and modify is granted under the MIT license
 
 using Test
-include("../src/<YOUR_FILE>.jl")
+# using MyPackage
 
-using Main.<YOUR_FILE>
 @testset "" begin
 
 end
