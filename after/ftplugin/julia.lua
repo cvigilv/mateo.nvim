dc2d704389f5af41a81025e9a71d0a19c0991a42
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-09-04 20:29:53 -0300

    style: format with stylua

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -1,8 +1,13 @@
 -- Tabulate behaviour
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth  = 4
-vim.opt.tabstop     = 4
-vim.opt.expandtab   = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 
 -- REPL
-vim.keymap.set("n", "<leader>R", ":!tmux split-window -h 'exec julia'<CR>", { noremap=true, desc = "REPL" })
+vim.keymap.set(
+  "n",
+  "<leader>R",
+  ":!tmux split-window -h 'exec julia'<CR>",
+  { noremap = true, desc = "REPL" }
+)
