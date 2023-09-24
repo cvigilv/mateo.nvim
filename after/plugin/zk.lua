8e828ca4991e2f0381c802829d7dd23eec3c7e38
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-09-23 21:21:02 -0300

    feat!(zk): Scrap `obisidian.nvim` for own manager

---
+++ after/plugin/zk.lua
@@ -0,0 +1,120 @@
+-- Setup Zettelkasten directory
+local zk = vim.fn.expand(os.getenv("ZETTELDIR"))
+local media = vim.fn.expand(zk .. "/meta/media")
+
+-- Telescope searchers
+local zk_theme = function(opts)
+  opts = opts or {}
+
+  local theme_opts = {
+    prompt_title = "",
+    preview_title = "Zettelkasten",
+    results_title = "",
+    prompt_prefix = "⌕ ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "vertical",
+    layout_config = {
+      horizontal = {},
+      vertical = {
+        prompt_position = "top",
+        results_width = 60,
+        results_length = 1,
+      },
+      width = 90,
+      height = 40,
+    },
+    path_display = { "truncate" },
+    winblend = 0,
+    border = {},
+    borderchars = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" },
+  }
+
+  return vim.tbl_deep_extend("force", theme_opts, opts)
+end
+
+local find_notes = function()
+  require("telescope.builtin").find_files(zk_theme({ cwd = zk }))
+end
+
+local search_notes = function()
+  require("telescope.builtin").live_grep(zk_theme({ cwd = zk }))
+end
+
+-- Keymaps
+local opts = { noremap = true, silent = false }
+vim.keymap.set(
+  "n",
+  "<leader>zf",
+  find_notes,
+  vim.tbl_extend("keep", opts, { desc = "Search notes" })
+)
+vim.keymap.set(
+  "n",
+  "<leader>zF",
+  search_notes,
+  vim.tbl_extend("keep", opts, { desc = "search inside notes" })
+)
+
+-- Note generator
+local function getdatelut(directory)
+  -- Get files associated to journal notes
+  local pattern = directory .. "/[0-9][0-9][0-9][0-9][0,1][0-9][0-3][0-9][a-zA-Z].md"
+  local notes = vim.split(vim.fn.glob(pattern), "\n", { trimempty = true })
+
+  -- Construct lookup table for last note ID per date
+  local lut_dates = {}
+  for _, notepath in ipairs(notes) do
+    -- Get date and note ID from filename
+    local date = vim.fn.fnamemodify(notepath, ":t:r"):sub(0, 8)
+    local id = vim.fn.fnamemodify(notepath, ":t:r"):sub(9)
+
+    -- Add note ID if not found or update to last note ID
+    if not vim.tbl_contains(lut_dates, date) then
+      lut_dates[date] = id
+    else
+      if id > lut_dates[date] then
+        lut_dates[date] = id
+      end
+    end
+  end
+
+  return lut_dates
+end
+
+local function nextnoteid(date, lut)
+  if vim.tbl_contains(vim.tbl_keys(lut), date) then
+    -- Get byte number of current last note ID
+    local charbyte = lut[date]:byte()
+
+    -- Jump from uppercase to lowercase IDs
+    if charbyte == 90 then
+      charbyte = 48
+    elseif charbyte == 122 then
+      charbyte = 64
+    end
+
+    -- Compute next node ID
+    local nextid = string.char(charbyte + 1)
+
+    return nextid
+  else
+    return "A"
+  end
+end
+
+local function nextnotename(date, lut)
+  return date .. nextnoteid(date, lut)
+end
+
+local function nextnotepath(dir, date, lut)
+  return dir .. "/" .. nextnotename(date, lut) .. ".md"
+end
+
+vim.keymap.set("n", "<leader>zc", function()
+  local currentdate = os.date("%Y%m%d", os.time())
+  vim.cmd("e " .. nextnotepath(zk, currentdate, getdatelut(zk)))
+end, vim.tbl_extend("keep", opts, { desc = "Create new notes" }))
--- lua/plugin/zk.lua
+++ lua/plugin/zk.lua
@@ -1,121 +1,39 @@
-local merge = function(a, b)
-  local c = {}
-  for k, v in pairs(a) do c[k] = v end
-  for k, v in pairs(b) do c[k] = v end
-  return c
-end
-
 return {
   -- obsidian {{{
   {
-    'epwalsh/obsidian.nvim',
+    "epwalsh/obsidian.nvim",
+    enabled = false,
     dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'hrsh7th/nvim-cmp',
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
     },
     ft = "markdown",
     lazy = true,
     event = "VeryLazy",
     keys = {
-      "<leader>zn",
-      "<leader>zp",
-      "<leader>zl",
-      "<leader>zj",
-      "<leader>zN",
-      "<leader>zP",
-      "<leader>zL",
-      "<leader>zJ",
+      "<leader>zc",
+      "<leader>zf",
+      "<leader>zg",
     },
     config = function()
-      require('obsidian').setup(
-        {
-          dir = os.getenv("ZETTELDIR"),
-          completion = {
-            nvim_cmp = true,
-          },
-          note_id_func = function(title)
-            if title ~= nil then
-              return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-            else
-              return string.char(math.random(65, 90))
-            end
-          end,
-          notes_subdir = ".",
-        }
-      )
+      local zk = os.getenv("ZETTELDIR")
+      require("obsidian").setup({
+        dir = zk,
+        notes_subdir = ".",
+        completion = {
+          nvim_cmp = true,
+          new_notes_location = "notes_subdir",
+        },
+        mappings = {},
+      })
 
-      vim.keymap.set(
-        "n",
-        ",zg",
-        "<CMD>ObsidianFollowLink<CR>",
-        { noremap = true }
-      )
-
-      -- Zettelkasten paths
-      local zk = vim.fn.expand(os.getenv("ZETTELDIR"))
-      local media = vim.fn.expand(zk .. "/media")
-
-      -- Zettelkasten-specific functions
-      -- local get_links = function()
-      -- end
-      --
-      -- local get_backlinks = function()
-      -- end
-      --
-      -- local get_alllinks = function()
-      -- end
-      --
-      -- local move_note = function()
-      -- end
-
-      -- Telescope searchers
-      local zk_theme = function(opts)
-        opts = opts or {}
-
-        local theme_opts = {
-          prompt_title = "",
-          preview_title = "Zettelkasten",
-          results_title = "",
-          prompt_prefix = "⌕ ",
-          selection_caret = "> ",
-          entry_prefix = "  ",
-          initial_mode = "insert",
-          selection_strategy = "reset",
-          sorting_strategy = "ascending",
-          layout_strategy = "vertical",
-          layout_config = {
-            horizontal = {
-            },
-            vertical = {
-              prompt_position = "top",
-              results_width = 60,
-              results_length = 1,
-            },
-            width = 90,
-            height = 40,
-          },
-          path_display = { "truncate" },
-          winblend = 0,
-          border = {},
-          borderchars = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" }
-        }
-
-        return vim.tbl_deep_extend("force", theme_opts, opts)
-      end
-
-      local find_notes = function()
-        require("telescope.builtin").find_files(zk_theme({ cwd = zk, }))
-      end
-
-      local search_notes = function()
-        require("telescope.builtin").live_grep(zk_theme({ cwd = zk, }))
-      end
-
-      -- Keymaps
-      local opts = { noremap = true, silent = false }
-
-      vim.keymap.set("n", "<leader>zn", find_notes, vim.tbl_extend("keep", opts, { desc = "Search notes" }))
-      vim.keymap.set("n", "<leader>zN", search_notes, vim.tbl_extend("keep", opts, { desc = "Search inside notes" }))
-    end
+      vim.keymap.set("n", "gf", function()
+        if require("obsidian").util.cursor_on_markdown_link() then
+          return "<cmd>ObsidianFollowLink<CR>"
+        else
+          return "gf"
+        end
+      end, { noremap = true, expr = true })
+    end,
   }, -- }}}
 }
