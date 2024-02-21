2236c45a289b03920d974eff24e8a9f0dc7b4402
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-02-20 23:21:04 -0300

    feat(autocmd): automatically create indent guides

--- lua/mateo/autocommands.lua
+++ lua/mateo/autocommands.lua
@@ -70,3 +70,22 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
     vim.opt.colorcolumn = table.concat(cols2blur, ",")
   end,
 })
+
+-- Update listchars
+local function update_lead()
+  local lcs = vim.opt_local.listchars:get()
+  local tab = vim.fn.str2list(lcs.tab)
+  local space = vim.fn.str2list(lcs.multispace or lcs.space)
+  local lead = { tab[1] }
+  for i = 1, vim.bo.tabstop - 1 do
+    lead[#lead + 1] = space[i % #space + 1]
+  end
+  vim.print("\"" .. vim.fn.list2str(lead) .. "\"")
+  vim.opt_local.listchars:append({ leadmultispace = vim.fn.list2str(lead) })
+end
+
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = { "listchars", "tabstop", "filetype" },
+  callback = update_lead,
+})
+vim.api.nvim_create_autocmd("VimEnter", { callback = update_lead, once = true })
