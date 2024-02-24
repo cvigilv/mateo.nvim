a9d50afe5acdb47a2239bdbb655cc101b888c4e8
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2024-02-24 20:10:17 -0300

    feat(zk): add img-clip for easier management of images in zk

--- lua/mateo/plugin/zk.lua
+++ lua/mateo/plugin/zk.lua
@@ -1,39 +1,35 @@
 return {
-  -- obsidian {{{
+  -- img-clip {{{
   {
-    "epwalsh/obsidian.nvim",
-    enabled = false,
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    ft = "markdown",
-    lazy = true,
-    event = "VeryLazy",
-    keys = {
-      "<leader>zc",
-      "<leader>zf",
-      "<leader>zg",
-    },
+    "HakonHarnes/img-clip.nvim",
     config = function()
-      local zk = os.getenv("ZETTELDIR")
-      require("obsidian").setup({
-        dir = zk,
-        notes_subdir = ".",
-        completion = {
-          nvim_cmp = true,
-          new_notes_location = "notes_subdir",
-        },
-        mappings = {},
-      })
+      local zk = vim.fn.expand(os.getenv("ZETTELDIR"))
 
-      vim.keymap.set("n", "gf", function()
-        if require("obsidian").util.cursor_on_markdown_link() then
-          return "<cmd>ObsidianFollowLink<CR>"
-        else
-          return "gf"
-        end
-      end, { noremap = true, expr = true })
+      require("img-clip").setup({
+        default = {
+          dir_path = "~/media/images/img-clip.nvim",
+          file_name = "%Y%m%d%H%M%S-" .. vim.fn.expand("%:t:r"),
+          use_absolute_path = true,
+          prompt_for_file_name = false,
+        },
+        dirs = {
+          [zk] = {
+            template = "![$CURSOR]($FILE_PATH)",
+            dir_path = "meta/media/" .. vim.fn.expand("%:t:r"),
+            file_name = vim.fn.expand("%:t:r") .. "-%Y%m%d%H%M%S",
+            use_absolute_path = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+              copy_images = true,
+              download_images = true,
+            },
+          },
+        },
+      })
     end,
+    keys = {
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+    },
   }, -- }}}
 }
