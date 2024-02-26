9100056092e0ca4efd6ddbe0c2bf4c5bf2ff9a49
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2024-02-26 12:50:20 -0300

    fix(utils): update to remove deprecated code

--- lua/mateo/utils.lua
+++ lua/mateo/utils.lua
@@ -5,11 +5,14 @@ local M = {}
 ---@param entry string Entry name to extract. Must be "foreground" or "background"
 ---@return string|nil Hexadecimal value of highlight group entry
 M.get_hl_group_hex = function(hl_group, entry)
-  local ok, hl = pcall(vim.api.nvim_get_hl_by_name, hl_group, true)
+  local ok, hl = pcall(vim.api.nvim_get_hl, hl_group, true)
   if ok then
     return string.format("#%06x", hl[entry])
   else
-    vim.notify("[ERROR] Highlight group doesn't exist!", vim.log.levels.ERROR)
+    vim.notify(
+      "[ERROR] Highlight group `" .. hl_group .. "` doesn't exist!",
+      vim.log.levels.ERROR
+    )
     return nil
   end
 end
