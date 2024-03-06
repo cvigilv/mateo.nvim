587c30675a8a6cce18aeb17582a70913cadc6fce
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-03-06 16:42:27 -0300

    feat(diagnostics): add keymaps

--- lua/mateo/keymaps.lua
+++ lua/mateo/keymaps.lua
@@ -1,3 +1,23 @@
 -- Terminal
 vim.api.nvim_set_keymap("t", "<leader><Esc>", "<C-\\><C-n>", { noremap = true })
 vim.api.nvim_set_keymap("t", "<leader>gf", ":e <cfile><CR>", { noremap = true })
+
+-- Diagnostics
+vim.keymap.set(
+  "n",
+  "[d",
+  function() vim.diagnostic.goto_prev({ wrap = false }) end,
+  { desc = "Go to previous diagnostics", noremap = true }
+)
+vim.keymap.set(
+  "n",
+  "]d",
+  function() vim.diagnostic.goto_next({ wrap = false }) end,
+  { desc = "Go to next diagnostics", noremap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>d",
+  function() vim.diagnostic.goto_next({ wrap = false }) end,
+  { desc = "Go to next diagnostics", noremap = true }
+)
