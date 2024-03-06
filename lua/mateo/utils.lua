eaf4c6f57d2f717aa0051561736181c9edd8b0dc
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-03-06 16:53:52 -0300

    feat: improve highlight group hex extractor

--- lua/mateo/utils.lua
+++ lua/mateo/utils.lua
@@ -2,12 +2,24 @@ local M = {}
 
 --- Get the hexadecimal value for a given highlight group and entry.
 ---@param hl_group string NeoVim highlight group name
----@param entry string Entry name to extract. Must be "foreground" or "background"
----@return string|nil Hexadecimal value of highlight group entry
+---@param entry string|nil Entry name to extract. Any of "foreground", "background", "fg" or "bg".
+---@return table|string|nil Hexadecimal value of highlight group entry
 M.get_hl_group_hex = function(hl_group, entry)
-  local ok, hl = pcall(vim.api.nvim_get_hl, hl_group, true)
+  -- Fix invalid entry names
+  if entry == "foreground" then
+    entry = "fg"
+  elseif entry == "background" then
+    entry = "bg"
+  end
+
+  -- Get highlight group colors
+  local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = hl_group, link = true, create = false })
   if ok then
-    return string.format("#%06x", hl[entry])
+    if entry ~= nil then
+      return string.format("#%06x", hl[entry])
+    else
+      return vim.tbl_map(function(e) return string.format("#%06x", e) end, hl)
+    end
   else
     vim.notify(
       "[ERROR] Highlight group `" .. hl_group .. "` doesn't exist!",
